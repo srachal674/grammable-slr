f77c684c474935c4ea9e83d1e9e0a525523e48e7
Shannon Rachal <srachal674@gmail.com>
2020-07-03 02:29:33 +0000

    Troubleshooting lesosn 10 issues

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -9,8 +9,22 @@ RSpec.describe GramsController, type: :controller do
 	end
 
 
-	describe "grams#index action" do
-		it "should successfully show teh new form" do
+	describe "grams#new action" do
+		it "should require users to be logged in" do
+			get :new
+			expect(response).to redirect_to new_user_session_path
+		end
+
+
+		it "should successfully show the new form" do
+			user = User.create(
+				email:                   'fakeuser@gmail.com',
+				password:                'secretPassword',
+				password_confirmation:   'secretPassword'
+			)
+			sign_in user
+
+
 			get :new
 			expect(response).to have_http_status(:success)
 		end
@@ -19,20 +33,46 @@ RSpec.describe GramsController, type: :controller do
 
 
 	describe "grams#create action" do
+
+
+		it "should require users to be logged in" do
+			post :create, params: { gram: { message: "Hello" } }
+			expect(response).to redirect_to new_user_session_path
+		end
+
+
 		it "should successfully create a new gram in our database" do
+			user = User.create(
+				email:                    'fakeuser@gmail.com',
+				password:                 'secretPassword',
+				password_confirmation:    'secretPassword'
+			)
+			sign_in user
+
+
 			post :create, params: { gram: { message: 'Hello!' } }
 			expect(response).to redirect_to root_path
 
 
 			gram = Gram.last
 			expect(gram.message).to eq("Hello!")
+			expect(gram.user).to eq(user)
 		end
-	end
 
 
-	it "should properly deal with validation errors" do
-		post :create, params: { gram: { message: '' } }
-		expect(response).to have_http_status(:unprocessable_entity)
-		expect(Gram.count).to eq 0
+		it "should properly deal with validation errors" do
+			user = User.create(
+				email:                    'fakeuser@gmail.com',
+				password:                 'secretPassword',
+				password_confirmation:    'secretPassword'
+			)
+
+
+			gram_count = Gram.count
+			post :create, params: { gram: { message: '' } }
+			expect(response).to have_http_status(:unprocessable_entity)
+			expect(gram.count).to eq Gram.count
+		end
+
 	end
 end
